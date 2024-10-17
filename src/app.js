e2657d9790230b7d3e83480bb200fc21381f3174
Venícios <veniciostenorio1@gmail.com>
2024-10-17 11:36:53 -0300

    adição de caixa de diálogo ao clicar nas opções

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const divAction = document.querySelector("services");
+const modal = document.querySelector("dialog");
+
+document.getElementById("pragas").addEventListener('click' , () => {
+    modal.showModal();
+});
+
+function mostrarModal() {
+    
+}
